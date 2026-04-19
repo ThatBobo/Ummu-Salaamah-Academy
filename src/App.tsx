f086b080cfaa1d4dd600d6d1009a0c8bb347d8d9
IamBobo <abdulsamad.arilewola@gmail.com>
2026-04-19 12:15:46 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import Index from './pages/Index';
+
 export default function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="Ummu-Salaamah-Academy">
       <Routes>
+        <Route path="/Ummu-Salaamah-Academy/" element={<Index />} />
       </Routes>
     </BrowserRouter>
   );
